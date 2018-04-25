7a0c7e467690bf8c047f3328e1c39efc71fed6fd
Steeve Andrian Salim <steevenz@ymail.com>
2018-04-25 21:24:36 +0700

    fixes error collector bugs

--- src/Traits/Collectors/ErrorCollectorTrait.php
+++ src/Traits/Collectors/ErrorCollectorTrait.php
@@ -27,7 +27,7 @@ trait ErrorCollectorTrait
      *
      * @var array
      */
-    protected $errors = [];
+    private $errors = [];
 
     // ------------------------------------------------------------------------
 
